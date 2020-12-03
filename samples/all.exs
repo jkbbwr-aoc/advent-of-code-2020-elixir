1d1bbfcb9e4ca454704733fa50a7972a1f6e9402
Jakob Bowyer <jkbbwr@gmail.com>
2020-12-03 11:14:13 +0000

    Added benchee all.

---
+++ samples/all.exs
@@ -0,0 +1,19 @@
+alias Aoc2020.{Day1, Day2, Day3}
+
+Benchee.run(
+  %{
+    "day 1 part 1" => fn -> Day1.part1() end,
+    "day 1 part 2" => fn -> Day1.part2() end,
+    "day 2 part 1" => fn -> Day2.part1() end,
+    "day 2 part 2" => fn -> Day2.part2() end,
+    "day 3 part 1" => fn -> Day3.part1() end,
+    "day 3 part 2" => fn -> Day3.part2() end,
+  },
+  formatters: [
+    {
+      Benchee.Formatters.Console,
+      [comparison: false, extended_statistics: true]
+    }
+  ],
+  parallel: 12,
+)
