f21a740f5bed58bbdd015452de2b1f7bff4a3f52
Jakob Bowyer <jkbbwr@gmail.com>
2020-12-13 12:17:37 +0000

    Day 13 solved. CRT can suck it.

--- lib/aoc2020/day13.ex
+++ lib/aoc2020/day13.ex
@@ -1,20 +1,54 @@
 defmodule Aoc2020.Day13 do
+  def parse("x"), do: :invalid
+  def parse(x), do: String.to_integer(x)
+
   def input() do
-    File.read!("input/day13.txt")
-    |> String.split("\n", trim: true)
+    input(File.read!("input/day13.txt"))
   end
 
   def input(text) do
-    text
-    |> String.split("\n", trim: true)
+    [depart_time, raw_schedule] = text
+                                  |> String.split("\n", trim: true)
+    schedule = raw_schedule
+               |> String.split(",", trim: true)
+               |> Enum.map(&parse/1)
+
+    {String.to_integer(depart_time), schedule}
   end
+
+  def find_nearest_departure(goal, schedule) when rem(goal, schedule) == 0, do: goal
+  def find_nearest_departure(goal, schedule), do: find_nearest_departure(goal + 1, schedule)
 
   def part1() do
-    input()
-
+    {goal, schedule} = input()
+    {id, closest} = schedule
+                    |> Enum.filter(&(&1 != :invalid))
+                    |> Enum.map(
+                         fn x ->
+                           {x, find_nearest_departure(goal, x)}
+                         end
+                       )
+                    |> Enum.min_by(fn {_, y} -> y end)
+    id * (closest - goal)
   end
 
+  def do_while(target, n, index, id) when rem(target + index, id) != 0, do: do_while(target + n, n, index, id)
+  def do_while(target, _n, _index, _id), do: target
+
   def part2() do
-    input()
+    {_, schedule} = input()
+    fields = schedule
+             |> Enum.with_index
+             |> Enum.filter(fn {x, _} -> x != :invalid end)
+
+    {target, _} = Enum.reduce(
+      fields,
+      {0, 1},
+      fn {id, index}, {target, n} ->
+        {do_while(target, n, index, id), n * id}
+      end
+    )
+
+    target
   end
 end
--- test/aoc2020/day13_test.exs
+++ test/aoc2020/day13_test.exs
@@ -3,10 +3,10 @@ defmodule Day13Test do
   alias Aoc2020.Day13
 
   test "day 13 part 1" do
-    assert Day13.part1() == 820
+    assert Day13.part1() == 138
   end
 
   test "day 13 part 2" do
-    assert Day13.part2() == 66614
+    assert Day13.part2() == 226845233210288
   end
 end
